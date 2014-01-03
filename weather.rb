4f4bad518322b16478f1a6b6486efb1ac12c634b
Gerardo Capiel <gerardoc@benetech.org>
2014-01-02 16:47:15 -0800

    bug fix in variable name

--- weather.rb
+++ weather.rb
@@ -26,7 +26,7 @@ open('http://api.wunderground.com/api/KEY/forecast10day/q/locid:01613388.json') 
   
   parsed_json['forecast']['simpleforecast']['forecastday'].each { |day|
   	if day['snow_allday']['in'] > 0
-  		output_msg += "#{day['date']['monthname_short']} #{day['date']['day']}: #{day['snow_allday']['in']}\" "
+  		snow_msg += "#{day['date']['monthname_short']} #{day['date']['day']}: #{day['snow_allday']['in']}\" "
   		snow_coming = 1
   	end
   }
